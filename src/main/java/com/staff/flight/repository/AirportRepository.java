88280f723b67c671871e1784b107a0c334c45ec8
Abel3581 <marzoa3581@gmail.com>
2022-05-06 00:06:38 -0300

    agregado Airport

---
+++ src/main/java/com/staff/flight/repository/AirportRepository.java
@@ -0,0 +1,9 @@
+package com.staff.flight.repository;
+
+import com.staff.flight.entity.Airport;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AirportRepository extends JpaRepository<Airport, Long> {
+}
