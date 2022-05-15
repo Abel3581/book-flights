5bfd21fc5b73a76a5d4520158cee0137095c10ef
Abel3581 <marzoa3581@gmail.com>
2022-05-14 23:04:02 -0300

    add deleted airport

--- src/main/java/com/staff/flight/service/abstraction/AirportService.java
+++ src/main/java/com/staff/flight/service/abstraction/AirportService.java
@@ -4,6 +4,8 @@ import com.staff.flight.entity.Airport;
 import com.staff.flight.entity.model.request.AirportRequest;
 import com.staff.flight.entity.model.response.AirportResponse;
 
+import javax.persistence.EntityNotFoundException;
+
 public interface AirportService {
     AirportResponse save(AirportRequest request);
     AirportResponse getById(long id);
@@ -11,4 +13,6 @@ public interface AirportService {
     AirportResponse getAirport(long id);
 
     Airport getAirportBy(long airportId);
+
+    void deleted(Long id)throws EntityNotFoundException;
 }
